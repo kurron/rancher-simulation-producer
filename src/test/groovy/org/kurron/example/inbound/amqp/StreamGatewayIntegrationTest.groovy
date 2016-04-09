4542d22b24152ec6e322ff9f2066e73a00731941
Ron Kurr <kurr@kurron.org>
2016-04-09 15:21:14 +0000

    Cut out more stuff.

--- src/test/groovy/org/kurron/example/inbound/amqp/StreamGatewayIntegrationTest.groovy
+++ src/test/groovy/org/kurron/example/inbound/amqp/StreamGatewayIntegrationTest.groovy
@@ -1,10 +1,24 @@
+/*
+ * Copyright (c) 2016. Ronald D. Kurr kurr@jvmguy.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.kurron.example.inbound.amqp
 
 import org.junit.experimental.categories.Category
 import org.kurron.categories.InboundIntegrationTest
 import org.kurron.example.Application
-import org.kurron.example.inbound.rest.RestCapable
-import org.kurron.traits.GenerationAbility
 import org.springframework.amqp.rabbit.core.RabbitAdmin
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.IntegrationTest
@@ -18,7 +32,7 @@ import spock.lang.Specification
 @Category( InboundIntegrationTest )
 @IntegrationTest
 @ContextConfiguration( classes = [Application,TestConfiguration], loader = SpringApplicationContextLoader )
-class StreamGatewayIntegrationTest extends Specification implements GenerationAbility, RestCapable {
+class StreamGatewayIntegrationTest extends Specification {
 
     @Autowired
     RabbitAdmin administrator
